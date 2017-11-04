043597d370c291bfd86c2d9be101a128b8e70650
Iyad Kuwatly <ikuwatly@gmail.com>
2017-11-04 16:32:57 -0400

    Add PropTypes

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,36 +1,40 @@
-import React, {Component} from 'react'
+import React from 'react'
+import PropTypes from 'prop-types'
 
-class BookGridItem extends Component {
-  render() {
-    return (
-      <ol className="books-grid">
-        {this.props.books.map((book) => (
-          <li key={book.id}>
-            <div className="book">
-              <div className="book-top">
-                <div className="book-cover" style={{
-                  width: 128,
-                  height: 193,
-                  backgroundImage: "url(" + (book.imageLinks.thumbnail || book.imageLinks.smallThumbnail) + ")"
-                }}></div>
-                <div className="book-shelf-changer">
-                  <select value={book.shelf} onChange={(e) => this.props.handleShelfChange(book, e.target.value)}>
-                    <option value="none" disabled>Move to...</option>
-                    <option value="currentlyReading">Currently Reading</option>
-                    <option value="wantToRead">Want to Read</option>
-                    <option value="read">Read</option>
-                    <option value="none">None</option>
-                  </select>
-                </div>
+function BookGridItem(props) {
+  return (
+    <ol className="books-grid">
+      {props.books.map((book) => (
+        <li key={book.id}>
+          <div className="book">
+            <div className="book-top">
+              <div className="book-cover" style={{
+                width: 128,
+                height: 193,
+                backgroundImage: "url(" + (book.imageLinks.thumbnail || book.imageLinks.smallThumbnail) + ")"
+              }}></div>
+              <div className="book-shelf-changer">
+                <select value={book.shelf} onChange={(e) => props.handleShelfChange(book, e.target.value)}>
+                  <option value="none" disabled>Move to...</option>
+                  <option value="currentlyReading">Currently Reading</option>
+                  <option value="wantToRead">Want to Read</option>
+                  <option value="read">Read</option>
+                  <option value="none">None</option>
+                </select>
               </div>
-              <div className="book-title">{book.title}</div>
-              <div className="book-authors">{book.authors.join(', ')}</div>
             </div>
-          </li>
-        ))}
-      </ol>
-    )
-  }
+            <div className="book-title">{book.title}</div>
+            <div className="book-authors">{book.authors.join(', ')}</div>
+          </div>
+        </li>
+      ))}
+    </ol>
+  )
+}
+
+BookGridItem.propTypes = {
+  books: PropTypes.array.isRequired,
+  handleShelfChange: PropTypes.func.isRequired
 }
 
 export default BookGridItem
