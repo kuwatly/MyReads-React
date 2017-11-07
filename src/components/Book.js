994f8aad31ebf672626df718eeb1ded4a41202a7
Iyad Kuwatly <ikuwatly@gmail.com>
2017-11-07 07:05:50 -0500

    Remove none shelf option

--- src/components/Book.js
+++ src/components/Book.js
@@ -17,7 +17,6 @@ function BookGridItem(props) {
               <option value="currentlyReading">Currently Reading</option>
               <option value="wantToRead">Want to Read</option>
               <option value="read">Read</option>
-              <option value="none">None</option>
             </select>
           </div>
         </div>
